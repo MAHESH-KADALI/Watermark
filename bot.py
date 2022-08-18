efec662c4150f32ef543eb6f97fdd70c70d67790
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-08-18 20:08:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ async def HelpWatermark(bot, cmd):
 	await cmd.reply_text(
 		text=Config.USAGE_WATERMARK_ADDER,
 		parse_mode="Markdown",
-		reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/AbirHasan2005"), InlineKeyboardButton("Support Group", url="https://t.me/DevsZone")], [InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")], [InlineKeyboardButton("Source Code", url="https://github.com/AbirHasan2005/Watermark-Bot")]]),
+		reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/mxnitro"), InlineKeyboardButton("Support Group", url="https://t.me/mxnitro")], [InlineKeyboardButton("Bots Channel", url="https://t.me/mxnitro")], [InlineKeyboardButton("owner", url="t.me/T3shaM")]]),
 		disable_web_page_preview=True
 	)
 

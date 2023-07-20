7348891ecf76473cf16ef99c6b03cf3ad71efd2b
Mx Nitro <95213910+Devilharsha@users.noreply.github.com>
2023-07-20 13:38:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ async def HelpWatermark(bot, cmd):
 	await cmd.reply_text(
 		text=Config.USAGE_WATERMARK_ADDER,
 		parse_mode="Markdown",
-		reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/mxnitro"), InlineKeyboardButton("Support Group", url="https://t.me/mxnitro")], [InlineKeyboardButton("Bots Channel", url="https://t.me/mxnitro")], [InlineKeyboardButton("owner", url="t.me/T3shaM")]]),
+		reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/mxnitro"), InlineKeyboardButton("Support Group", url="https://t.me/NinjaUniverseChat")], [InlineKeyboardButton("Updates ✨", url="https://t.me/Ninjauniverse")], [InlineKeyboardButton("META💖", url="t.me/METAXUPDATES")]]),
 		disable_web_page_preview=True
 	)
 
@@ -395,7 +395,7 @@ async def button(bot, cmd: CallbackQuery):
 				user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), cmd.message.chat.id)
 				if user.status == "kicked":
 					await cmd.message.edit(
-						text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/DevsZone).",
+						text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/NinjauniverseChat).",
 						parse_mode="markdown",
 						disable_web_page_preview=True
 					)
@@ -418,7 +418,7 @@ async def button(bot, cmd: CallbackQuery):
 				return
 			except Exception:
 				await cmd.message.edit(
-					text="Something went Wrong. Contact my [Support Group](https://t.me/DevsZone).",
+					text="Something went Wrong. Contact my [Support Group](t.me/Ninjauniverse).",
 					parse_mode="markdown",
 					disable_web_page_preview=True
 				)
@@ -426,7 +426,7 @@ async def button(bot, cmd: CallbackQuery):
 		await cmd.message.edit(
 			text=Config.USAGE_WATERMARK_ADDER,
 			parse_mode="Markdown",
-			reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/AbirHasan2005"), InlineKeyboardButton("Support Group", url="https://t.me/DevsZone")], [InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]]),
+			reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer", url="https://t.me/MXNITRO"), InlineKeyboardButton("Support Group", url="https://t.me/NinjaUniverseChat")], [InlineKeyboardButton("Bots Channel", url="https://t.me/Ninjauniverse")]]),
 			disable_web_page_preview=True
 		)
 
@@ -443,7 +443,7 @@ async def button(bot, cmd: CallbackQuery):
 				user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), cmd.message.chat.id)
 				if user.status == "kicked":
 					await cmd.message.edit(
-						text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/DevsZone).",
+						text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/NINJAUNIVERSECHAT).",
 						parse_mode="markdown",
 						disable_web_page_preview=True
 					)
@@ -466,7 +466,7 @@ async def button(bot, cmd: CallbackQuery):
 				return
 			except Exception:
 				await cmd.message.edit(
-					text="Something went Wrong. Contact my [Support Group](https://t.me/DevsZone).",
+					text="Something went Wrong. Contact my [Support Group](https://t.me/NinjaUniverseChat).",
 					parse_mode="markdown",
 					disable_web_page_preview=True
 				)
